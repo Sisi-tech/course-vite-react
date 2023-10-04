6db3aef3562d099911453262defde7e76fca684b
sisi wang <sisiwang242@gmail.com>
2023-10-03 21:31:45 -0400

    updated form.jsx

--- src/form.jsx
+++ src/form.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 
 const ControlledInputs = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
     return (
-        <form>
+        <form className="form" onSubmit={(e)=>e.preventDefault()}>
             <h4>Controlled Inputs</h4>
             <div className="form-row">
                 <label className="form-label" htmlFor="name">Name</label>
-                <input type="text" id="name" />
+                <input 
+                type="text" 
+                id="name" 
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                />
             </div>
             <div className="form-row">
                 <label className="form-label" htmlFor="email">Email</label>
-                <input type="text" id="email" />
+                <input 
+                type="text" 
+                id="email" 
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
             </div>
             <div className="form-row">
                 <label className="form-label" htmlFor="password">Password</label>
